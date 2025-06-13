705e73165809b55d3bc6443f05f540a081299eac
DanielMoore98 <daniel.m@trabemco.net>
2025-06-13 14:57:16 +0200

    grasshopper-terminal-game-combat-function

---
+++ 8kyu/grasshopper-terminal-game-combat-function.js
@@ -0,0 +1,16 @@
+//❗DESCRIPTION
+// Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+
+//TESTS:
+// Test.assertEquals(combat(100, 5), 95);
+// Test.assertEquals(combat(92, 8), 84);
+// Test.assertEquals(combat(20, 30), 0, "Health cannot go below 0");
+
+//💡SOLUTION:
+function combat(health, damage) {
+    return health - damage > 0 ? health - damage : 0
+}
+
+
+
+
